a95fbaa60b7d52f6c6c16885ba62064d4385a903
Soil_L <Soil_L@163.com>
2025-02-21 16:47:29 +0800

    begin to train PushT

--- dataset.py
+++ dataset.py
@@ -9,46 +9,19 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 from torchvision.datasets import CelebA
 import zipfile
+import numpy as np
 
 
 # Add your custom dataset class here
-class MyDataset(Dataset):
-    def __init__(self):
-        pass
-    
-    
-    def __len__(self):
-        pass
-    
-    def __getitem__(self, idx):
-        pass
-
-
-class MyCelebA(CelebA):
-    """
-    A work-around to address issues with pytorch's celebA dataset class.
-    
-    Download and Extract
-    URL : https://drive.google.com/file/d/1m8-EBPgi5MRubrm6iQjafK2QMHDBMSfJ/view?usp=sharing
-    """
-    
-    def _check_integrity(self) -> bool:
-        return True
-    
-    
-
-class OxfordPets(Dataset):
-    """
-    URL = https://www.robots.ox.ac.uk/~vgg/data/pets/
-    """
+class myDataset(Dataset):
     def __init__(self, 
                  data_path: str, 
                  split: str,
                  transform: Callable,
                 **kwargs):
-        self.data_dir = Path(data_path) / "OxfordPets"        
+        self.data_dir = Path(data_path) / "observation"        
         self.transforms = transform
-        imgs = sorted([f for f in self.data_dir.iterdir() if f.suffix == '.jpg'])
+        imgs = sorted([f for f in self.data_dir.iterdir() if f.suffix == '.png'])
         
         self.imgs = imgs[:int(len(imgs) * 0.75)] if split == "train" else imgs[int(len(imgs) * 0.75):]
     
@@ -62,6 +35,7 @@ class OxfordPets(Dataset):
             img = self.transforms(img)
         
         return img, 0.0 # dummy datat to prevent breaking 
+
 
 class VAEDataset(LightningDataModule):
     """
@@ -151,6 +125,90 @@ class VAEDataset(LightningDataModule):
             download=False,
         )
 #       ===============================================================
+        
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.train_batch_size,
+            num_workers=self.num_workers,
+            shuffle=True,
+            pin_memory=self.pin_memory,
+        )
+
+    def val_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.val_batch_size,
+            num_workers=self.num_workers,
+            shuffle=False,
+            pin_memory=self.pin_memory,
+        )
+    
+    def test_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
+        return DataLoader(
+            self.val_dataset,
+            batch_size=144,
+            num_workers=self.num_workers,
+            shuffle=True,
+            pin_memory=self.pin_memory,
+        )
+     
+class PushTDataset(LightningDataModule):
+    """
+    PyTorch Lightning data module 
+
+    Args:
+        data_dir: root directory of your dataset.
+        train_batch_size: the batch size to use during training.
+        val_batch_size: the batch size to use during validation.
+        patch_size: the size of the crop to take from the original images.
+        num_workers: the number of parallel workers to create to load data
+            items (see PyTorch's Dataloader documentation for more details).
+        pin_memory: whether prepared items should be loaded into pinned memory
+            or not. This can improve performance on GPUs.
+    """
+
+    def __init__(
+        self,
+        data_path: str,
+        train_batch_size: int = 8,
+        val_batch_size: int = 8,
+        patch_size: Union[int, Sequence[int]] = (256, 256),
+        num_workers: int = 0,
+        pin_memory: bool = False,
+        **kwargs,
+    ):
+        super().__init__()
+
+        self.data_dir = data_path
+        self.train_batch_size = train_batch_size
+        self.val_batch_size = val_batch_size
+        self.patch_size = patch_size
+        self.num_workers = num_workers
+        self.pin_memory = pin_memory
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        train_transforms = transforms.Compose([transforms.RandomHorizontalFlip(),
+                                            #   transforms.CenterCrop(148),
+                                              transforms.Resize(self.patch_size),
+                                              transforms.ToTensor(),])
+        
+        val_transforms = transforms.Compose([transforms.RandomHorizontalFlip(),
+                                            # transforms.CenterCrop(148),
+                                            transforms.Resize(self.patch_size),
+                                            transforms.ToTensor(),])
+        
+        self.train_dataset = myDataset(
+            self.data_dir,
+            split='train',
+            transform=train_transforms,
+        )
+        
+        self.val_dataset = myDataset(
+            self.data_dir,
+            split='test',
+            transform=val_transforms,
+        )
         
     def train_dataloader(self) -> DataLoader:
         return DataLoader(
